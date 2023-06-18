1887aab495702aca6729ee6ddad7bf54ec987499
Chinedu Olebu <cgolebu@gmail.com>
2023-06-18 15:22:43 -0700

    modified script.groovy

--- script.groovy
+++ script.groovy
@@ -9,8 +9,8 @@ def deployApp(){
     echo "===============Deploying Application================="
     withCredentials([usernamePassword(credentialsId: 'nexus-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]){
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin 159.203.28.198:8082"
-        sh "docker tag my-app:1.0 159.203.28.198:8081/my-app:1.0"
-        sh "docker push 159.203.28.198:8081/my-app:1.0"
+        sh "docker tag my-app:1.0 159.203.28.198:8082/my-app:1.0"
+        sh "docker push 159.203.28.198:8082/my-app:1.0"
     }
 } 
 return this
