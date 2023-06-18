ba5cc93cb1061a419dd640e40dd6a90ff5bcfa54
Chinedu Olebu <cgolebu@gmail.com>
2023-06-18 15:12:49 -0700

    modified script.groovy | using the nexus repo

--- script.groovy
+++ script.groovy
@@ -1,15 +1,16 @@
 def buildApp(){
     echo "===============Building Application==============="
-    sh "docker build --tag golebu2020/maven-repo:django-payment-app-1.0 ."
+    sh "docker build --tag my-app:1.0 ."
 }
 return this
 
 
 def deployApp(){
     echo "===============Deploying Application================="
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]){
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh "docker push golebu2020/maven-repo:django-payment-app-1.0"
+    withCredentials([usernamePassword(credentialsId: 'nexus-credentials', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]){
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin 159.203.28.198:8082"
+        sh "docker tag my-app:1.0 159.203.28.198:8081/my-app:1.0"
+        sh "docker push 159.203.28.198:8081/my-app:1.0"
     }
 } 
 return this
